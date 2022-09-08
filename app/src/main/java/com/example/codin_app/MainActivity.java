74f220459f146b63925d4cc31127e06994bcb38e
EMMANUEL <emmanuelsho95@gmail.com>
2022-09-08 10:24:31 -0700

    a demo of codin project

---
+++ app/src/main/java/com/example/codin_app/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.codin_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://learncodin.com/");
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+    }
+
+    private  class MywebCliebt extends WebViewClient{
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+        }
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+            return super.shouldOverrideUrlLoading(view, request);
+        }
+    }
+
+
+    @Override
+    public void onBackPressed() {
+
+        if (webView.isFocused()&&webView.canGoBack()){
+            webView.goBack();
+        }else{
+            super.onBackPressed();
+
+        }
+
+    }
+}
